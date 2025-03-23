51919f5776e98fe4c2fedd6f1ed8d0b3abda24ef
Steven Woolgar <steven_Woolgar@Woolsoft.com>
2025-03-22 21:51:13 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,15 +3,12 @@ import PackageDescription
 
 let package = Package(
     name: "ColorCode",
-    platforms: [
-        .macOS(.v14),
-    ],
+    platforms: [.macOS(.v15)],
     products: [
         .library(name: "ColorCode", targets: ["ColorCode"]),
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-numerics", from: Version(1, 0, 2)),
-        .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", from: Version(0, 57, 0)),
     ],
     targets: [
         .target(name: "ColorCode"),
@@ -21,13 +18,3 @@ let package = Package(
         ])
     ]
 )
-
-
-for target in package.targets {
-    target.plugins = [
-        .plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins"),
-    ]
-    target.swiftSettings = [
-        .enableUpcomingFeature("ExistentialAny"),
-    ]
-}
